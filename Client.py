6cef5bb2dcaaadb5a5e2cbbf4125d3adcc916778
yangshouguo <891584158@qq.com>
2017-08-21 11:37:11 +0800

    new Client

--- Client.py
+++ Client.py
@@ -4,17 +4,26 @@
 
 
 from socket import *
-import random
-host = 'localhost'
+import sys
+host = '10.10.2.191'
 port = 7890
 sock_server = (host,port)
 
+def print_help():
+    
+    data = 'use this script as :python Client \'$6$9WbBd61T$bGOeNO09qbTDxsLLghNqoKPRlA.BzZQVOwvCs7CtfhX5y/diF1/pIQm.BsuhrzlP8BHcgC8YO8W.RH4L9dpYA1\' 1'
+    print data
+
 def main():
+    if len(sys.argv)<3:
+        print_help()
+        return
+
     try:
         client_sock = socket(AF_INET, SOCK_STREAM)
         client_sock.connect(sock_server)
-        data = '$6$9WbBd61T$bGOeNO09qbTDxsLLghNqoKPRlA.BzZQVOwvCs7CtfhX5y/diF1/pIQm.BsuhrzlP8BHcgC8YO8W.RH4L9dpYA1'
-        data += ' 1'
+        data = sys.argv[1]
+        data+=' '+sys.argv[2]
         print 'sending data:',data
         client_sock.sendall(data)
         print client_sock.recv(1024)
